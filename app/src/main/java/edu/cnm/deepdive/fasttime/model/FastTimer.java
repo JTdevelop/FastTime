93e7d907424f3f54bf184ab4e48d7f50a95b0006
Jay T <torrcorp@gmail.com>
2018-06-27 20:27:19 -0600

    entity classes added

--- app/src/main/java/edu/cnm/deepdive/fasttime/model/FastTimer.java
+++ app/src/main/java/edu/cnm/deepdive/fasttime/model/FastTimer.java
@@ -1,8 +1,49 @@
 package edu.cnm.deepdive.fasttime.model;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
+import java.util.TimerTask;
+
+@Entity(
+    indices = {
+        @Index(value = {"start"}, unique = true),
+        @Index(value = {"stop"}, unique = true)
+    }
+)
 
 public class FastTimer {
 
+  @PrimaryKey(autoGenerate = true)
+  @ColumnInfo(name = "fast_time_id")
+  private long id;
+
+  private long start;
+  private long stop;
+
+  public long getStart() {
+    return start;
+  }
+
+  public void setStart(long start) {
+    this.start = start;
+  }
+
+  public long getStop() {
+    return stop;
+  }
+
+  public void setStop(long stop) {
+    this.stop = stop;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
 
 }
