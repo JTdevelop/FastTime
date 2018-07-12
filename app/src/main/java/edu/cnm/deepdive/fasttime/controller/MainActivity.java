fa13dcb3606734622640e9fb0814d9748687e6cc
Jay T <torrcorp@gmail.com>
2018-07-11 21:56:44 -0600

    updated fast xml

--- app/src/main/java/edu/cnm/deepdive/fasttime/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/fasttime/controller/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.fasttime.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
@@ -7,6 +8,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import edu.cnm.deepdive.fasttime.R;
