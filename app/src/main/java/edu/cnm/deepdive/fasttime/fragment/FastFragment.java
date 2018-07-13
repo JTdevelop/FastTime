aab162acce0295a7a7f9ca7066e20c3255e757c2
Jay T <torrcorp@gmail.com>
2018-07-12 21:36:31 -0600

    updated fast fragment

--- app/src/main/java/edu/cnm/deepdive/fasttime/fragment/FastFragment.java
+++ app/src/main/java/edu/cnm/deepdive/fasttime/fragment/FastFragment.java
@@ -1,21 +1,28 @@
 package edu.cnm.deepdive.fasttime.fragment;
 
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 import edu.cnm.deepdive.fasttime.R;
 import edu.cnm.deepdive.fasttime.fragment.DateTimePickerFragment.Mode;
 import edu.cnm.deepdive.fasttime.fragment.DateTimePickerFragment.OnChangeListener;
+import edu.cnm.deepdive.fasttime.model.FastDatabase;
+import edu.cnm.deepdive.fasttime.model.GymTimer;
 import java.util.Calendar;
+import java.util.List;
 
 public class FastFragment extends Fragment {
 
@@ -77,6 +84,58 @@ public class FastFragment extends Fragment {
     public void onChange(Calendar calendar) {
       checkedByCode = true;
       fast.setChecked(true);
+    }
+  }
+
+  private static class GetTimers extends AsyncTask<Context, Void, List<GymTimer>> {
+
+    private Spinner spinner;
+    private Context context;
+
+    private GetTimers(Spinner spinner) {
+      this.spinner = spinner;
+    }
+
+    @Override
+    protected List<GymTimer> doInBackground(Context... contexts) {
+      this.context = contexts[0];
+      return FastDatabase.getInstance(contexts[0]).getGymTimerDao().select();
+    }
+
+    @Override
+    protected void onPostExecute(List<GymTimer> gymTimers) {
+      ArrayAdapter<GymTimer> adapter = new ArrayAdapter<>(
+          context, android.R.layout.simple_list_item_1, gymTimers);
+      spinner.setAdapter(adapter);
+      spinner = null;
+      context = null;
+    }
+
+    @Override
+    protected void onCancelled(List<GymTimer> gymTimers) {
+      super.onCancelled(gymTimers);
+      spinner = null;
+      context = null;
+    }
+  }
+
+  private class Timer extends Thread {
+
+    @Override
+    public void run() {
+      while (running) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          // DO NOTHING!
+        }
+        getActivity().runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            update();
+          }
+        });
+      }
     }
   }
 
